f61a8b8672db2a2bee7762919484ac851ee4cae1
Johannes Mensalo <jimens@utu.fi>
2022-12-15 19:14:34 +0200

    socket sending data from mongo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
 import Home from "./components/Home.jsx";
 import { io } from "socket.io-client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [data, setData] = useState([])
-  const socket = io();
 
-  socket.on("connect", () => {
-    console.log(socket.id);
-    socket.emit("ready");
-  });
+  const sockets = () => {
+    const socket = io();
+    socket.on("connect", () => {
+      console.log("Client: " + socket.id);
+      socket.emit("ready");
 
-  socket.on("violationData", (data) => {
-    console.log("Recieved ", data.data);
-    setData(data);
-  });
+      socket.on("violationData", (data) => {
+        console.log("Recieved ", data);
+        setData(data);
+      });
+    });
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+  }
+
+  useEffect(() => {
+    sockets()
+  }, [])
+
 
   return (<Home violations={data} />);
 }
