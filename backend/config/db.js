e9a4f88b4b42c3ea3a6dc420a9ba4c60f00c83fc
Johannes Mensalo <jimens@utu.fi>
2022-12-16 18:21:02 +0200

    updated postinstall script

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const options = { useNewUrlParser: true }
     mongoose.set('strictQuery', true)
-    const conn = await mongoose.connect(process.env.MONGO_URI, options);
+    const conn = await mongoose.connect(process.env.DATABASE_URL, options);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     process.exit(1);
