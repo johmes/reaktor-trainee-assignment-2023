295c8915d6508650a44dee4612885db6a1c4d064
Johannes <74429647+johmes@users.noreply.github.com>
2022-12-27 02:40:43 +0200

    Changed to es5 function

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from "../styles/Home.module.css"
 import Card from "./Card";
 
-const Home = (props) => {
+function Home(props) {
   return (
     <div className={styles.container}>
       <header>
